04ff393dca4882431235776e7ed476575fcd2d5a
Dhruvit <sonidhruvit7000@gmail.com>
2026-04-19 14:48:04 +0530

    this is first time

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-black ">
       <h1 className="text-4xl font-bold text-white mt-40">
-        Tailwind v4 Working 🚀
+        Dhruvit Profile using React+Tailwind v4 Working 🚀
       </h1>
     </div>
   );
