8c5fc7664f317f3e70fa8e455be13cafeec49524
Dhruvit <sonidhruvit7000@gmail.com>
2026-04-19 14:46:46 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black ">
+      <h1 className="text-4xl font-bold text-white mt-40">
+        Tailwind v4 Working 🚀
+      </h1>
+    </div>
+  );
+}
